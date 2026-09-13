2d36d4ce6fce7e838548162190249aaabe49f9de
leandro-msp <leandromarcjt@gmail.com>
2026-09-13 15:50:30 -0300

    index

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP </title>
+</head>
+<body style="background-color: #D7EBF1;">
+    <h1 style="color:blueviolet">Curso PHP e MySQL: POO + Projetos</h1>
+    <ul>
+        <h1 style="color: #03bb85;">Fundamentos</h1>
+         <h4 style="color: red;">AVISO:<br>Tópicos "Variáveis, Tratamento String, e Arrays" a quebra de linha está formatada para saídas no terminal(exceto o arquivo de constantes)</h4>
+
+        <h3>Variáveis e Constantes</h3>
+        <li><a href="01-fundamentos-php/01-variaveis-e-constantes/variaveis.php">Variáveis</a></li>
+        <li><a href="01-fundamentos-php/01-variaveis-e-constantes/constantes.php">Constantes</a></li>
+
+        <h3>Tratamento String</h3>
+        <li><a href="01-fundamentos-php/02-tratamento-string/string1.php">Tratamento de String(Trim)</a></li>
+        <li><a href="01-fundamentos-php/02-tratamento-string/string2.php">Tratamento de String(Upper, Lower, ucfirst, ucwords)</a></li>
+        <li><a href="01-fundamentos-php/02-tratamento-string/novasfuncoes.php">Tratamento de String(Contains, Start_With, Ends_With)</a></li>
+        
+        <h3>Arrays</h3>
+        <li><a href="01-fundamentos-php/03-arrays/arrays.php">Arrays Indexado e associativo</a></li>
+        <li><a href="01-fundamentos-php/03-arrays/arrays2.php">Manipulando Arrays</a></li>
+
+        <h3>Operadores</h3>
+        <li><a href="01-fundamentos-php/04-operadores/operadores.php">Operadores Relacionais</a></li>
+        <li><a href="01-fundamentos-php/04-operadores/logicos.php">Operadores Lógicos</a></li>
+        <li><a href="01-fundamentos-php/04-operadores/aritimeticos.php">Operadores Aritiméticos</a></li>
+
+        <h3>Estrutura de Controle</h3>
+        <li><a href="01-fundamentos-php/05-estrutura_controle/estruturaControle.php">If e Else</a></li>
+        <li><a href="01-fundamentos-php/05-estrutura_controle/switch.php">Switch-Case</a></li>
+        <li><a href="01-fundamentos-php/05-estrutura_controle/match.php">Match</a></li>
+
+        <h3>Estrutura de Repetição</h3>
+        <li><a href="01-fundamentos-php/06-estruturaRepeticao/loop.php">For, While e Foreach</a></li>
+        <li><a href="01-fundamentos-php/06-estruturaRepeticao/praticando.php">Revisão</a></li>
+
+        <h3>Funções</h3>
+         <li><a href="02-funcoes/funcoes.php">Criando e chamando uma Função (não formatado para navegador)</a></li>
+
+        <h2 style="color: rgb(161, 8, 8);">Exercícios</h2>
+        <ol>
+            <h3>Fundamentos</h3>
+            <li><a href="01-fundamentos-php/exercicios/exercicio1/versao_navegador.php">Recuperando dados</a></li>
+            <li><a href="01-fundamentos-php/exercicios/exercicio2/versao_navegador.php">Cálculo de Desconto</a></li>
+            <li><a href="01-fundamentos-php/exercicios/exercicio3/versaonavegador.php">Par ou Ímpar</a></li>
+            <li><a href="01-fundamentos-php/exercicios/exercicio4/alunos.php">Relatório - Alunos </a></li>
+            <h3>Funções</h3>
+            <li><a href="02-funcoes/exercicios/exercicio5/funcao.php">Função - Cálculo Média</a></li>
+            <li><a href="02-funcoes/exercicios/exercicio7/relatorioVendas.php">Função - Ler Arquivo CSV (não está formatado para saída no navegador, somente terminal)</a></li>
+        </ol>
+    </ul>
+</body>
+</html>
